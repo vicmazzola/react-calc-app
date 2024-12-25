0c482c894481e3449e440b9c1fe4a2349d1f2da3
Victor Mazzola <victormazzola2019@gmail.com>
2024-12-25 19:37:25 -0300

    test all

--- calculator/src/App.jsx
+++ calculator/src/App.jsx
@@ -129,6 +129,16 @@ function evaluate({currentOperand, previousOperand, operation}) {
     return computation.toString()
 }
 
+const INTEGER_FORMATTER = new Intl.NumberFormat("en-us", {
+    maximumFractionDigits: 0,
+})
+
+function formatOperand(operand) {
+    if (operand == null) return
+    const [integer, decimal] = operand.split(".")
+    if (decimal == null) return INTEGER_FORMATTER.format(integer)
+    return `${INTEGER_FORMATTER.format(integer)}.${decimal}`
+}
 
 function App() {
 
@@ -140,11 +150,20 @@ function App() {
     return (
         <div className="calculator-grid">
             <div className="output">
-                <div className="previous-operand">{previousOperand} {operation}</div>
-                <div className="current-operand">{currentOperand}</div>
+                <div className="previous-operand">
+                    {formatOperand(previousOperand)} {operation}
+                </div>
+                <div className="current-operand">{formatOperand(currentOperand)}</div>
             </div>
-            <button className="span-two" onClick={() => dispatch({type: ACTIONS.CLEAR})}>AC</button>
-            <button>DEL</button>
+            <button
+                className="span-two"
+                onClick={() => dispatch({type: ACTIONS.CLEAR})}
+            >
+                AC
+            </button>
+            <button onClick={() => dispatch({type: ACTIONS.DELETE_DIGIT})}>
+                DEL
+            </button>
             <OperationButton operation="÷" dispatch={dispatch}/>
             <DigitButton digit="1" dispatch={dispatch}/>
             <DigitButton digit="2" dispatch={dispatch}/>
