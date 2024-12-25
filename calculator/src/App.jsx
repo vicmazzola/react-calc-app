4340a797d09b936467550a4db92b8712a21151eb
Victor Mazzola <victormazzola2019@gmail.com>
2024-12-25 09:03:26 -0300

    start calculator project react vite

---
+++ calculator/src/App.jsx
@@ -0,0 +1,21 @@
+function App () {
+    return (
+        <div className="calculator-grid">
+            <div className="output">
+                <div className="previous-operand"></div>
+                <div className="current-operand"></div>
+            </div>
+            <button className="span-two">AC</button>
+            <button>DEL</button>
+            <button>÷</button>
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <button>7</button>
+            <button>8</button>
+        </div>
+    )
+}
