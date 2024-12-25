73696c049569a7d44414c6c89376f924110d361f
Victor Mazzola <victormazzola2019@gmail.com>
2024-12-25 20:06:38 -0300

    vite plugin pwa

--- calculator/vite.config.js
+++ calculator/vite.config.js
@@ -1,7 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'React Calculator',
+        short_name: 'Calculator',
+        description: 'A simple calculator app built with React',
+        theme_color: '#00AAFF',
+        background_color: '#00FF6C',
+        display: 'standalone',
+        icons: [
+          {
+            src: '/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+});
