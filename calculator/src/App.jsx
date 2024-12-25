881c50b5754cf3779d2d0677efbf85e76f2df29a
Victor Mazzola <victormazzola2019@gmail.com>
2024-12-25 19:25:23 -0300

    add ACTIONS.DELETE_DIGIT

--- calculator/src/App.jsx
+++ calculator/src/App.jsx
@@ -16,6 +16,13 @@ export const ACTIONS = {
 function reducer(state, {type, payload}) {
     switch (type) {
         case ACTIONS.ADD_DIGIT:
+            if (state.overwrite) {
+                return {
+                    ...state,
+                    currentOperand: payload.digit,
+                    overwrite: false,
+                }
+            }
             if (payload.digit === "0" && state.currentOperand === "0") {
                 return state
             }
@@ -29,6 +36,15 @@ function reducer(state, {type, payload}) {
         case ACTIONS.CHOOSE_OPERATION:
             if (state.currentOperand == null && state.previousOperand == null) {
                 return state
+            }
+
+            if (state.currentOperand == null) {
+
+                return {
+                    ...state,
+                    operation:
+                    payload.operation,
+                }
             }
 
             if (state.currentOperand == null) {
@@ -55,10 +71,43 @@ function reducer(state, {type, payload}) {
             }
         case ACTIONS.CLEAR:
             return {}
+        case ACTIONS.DELETE_DIGIT:
+            if (state.overwrite) {
+                return {
+                    ...state,
+                    overwrite: false,
+                    currentOperand: null,
+                }
+            }
+            if (state.currentOperand == null) return state
+            if (state.currentOperand.length === 1) {
+                return {...state, currentOperand: null}
+            }
+
+            return {
+                ...state,
+                currentOperand: state.currentOperand.slice(0, -1),
+            }
+        case ACTIONS.EVALUATE:
+            if (
+                state.operation == null ||
+                state.currentOperand == null ||
+                state.previousOperand == null
+            ) {
+                return state
+            }
+
+            return {
+                ...state,
+                overwrite: true,
+                previousOperand: null,
+                operation: null,
+                currentOperand: evaluate(state),
+            }
     }
 }
 
-function evaluate({ currentOperand, previousOperand, operation}) {
+function evaluate({currentOperand, previousOperand, operation}) {
     const prev = parseFloat(previousOperand)
     const current = parseFloat(currentOperand)
     if (isNaN(prev) || isNaN(current)) return ""
@@ -77,6 +126,7 @@ function evaluate({ currentOperand, previousOperand, operation}) {
             computation = prev / current
             break
     }
+    return computation.toString()
 }
 
 
@@ -110,7 +160,12 @@ function App() {
             <OperationButton operation="-" dispatch={dispatch}/>
             <DigitButton digit="." dispatch={dispatch}/>
             <DigitButton digit="0" dispatch={dispatch}/>
-            <button className="span-two">=</button>
+            <button
+                className="span-two"
+                onClick={() => dispatch({type: ACTIONS.EVALUATE})}
+            >
+                =
+            </button>
 
         </div>
     )
