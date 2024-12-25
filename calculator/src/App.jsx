83a1a7664887fec64ef10dc9e78d80988c510017
Victor Mazzola <victormazzola2019@gmail.com>
2024-12-25 09:53:52 -0300

    test style calculator

--- calculator/src/App.jsx
+++ calculator/src/App.jsx
@@ -4,8 +4,8 @@ function App () {
     return (
         <div className="calculator-grid">
             <div className="output">
-                <div className="previous-operand"></div>
-                <div className="current-operand"></div>
+                <div className="previous-operand">123, 234 * </div>
+                <div className="current-operand"> 123, 234</div>
             </div>
             <button className="span-two">AC</button>
             <button>DEL</button>
@@ -13,9 +13,11 @@ function App () {
             <button>1</button>
             <button>2</button>
             <button>3</button>
+            <button>*</button>
             <button>4</button>
             <button>5</button>
             <button>6</button>
+            <button>+</button>
             <button>7</button>
             <button>8</button>
             <button>9</button>
