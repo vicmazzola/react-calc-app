064655734d67a4087c8d915f6fb0d95fb4923c71
Victor Mazzola <victormazzola2019@gmail.com>
2024-12-25 09:56:15 -0300

    add useReducer

--- calculator/src/App.jsx
+++ calculator/src/App.jsx
@@ -1,11 +1,19 @@
 import "./index.css";
+import {useReducer} from "react";
+
+function reducer(state, action) {
+
+}
 
 function App () {
+
+    const [{currentOperand, previousOperand, operation}, dispatch] = useReducer(reducer, {})
+
     return (
         <div className="calculator-grid">
             <div className="output">
-                <div className="previous-operand">123, 234 * </div>
-                <div className="current-operand"> 123, 234</div>
+                <div className="previous-operand">{previousOperand} {operation}</div>
+                <div className="current-operand">{currentOperand}</div>
             </div>
             <button className="span-two">AC</button>
             <button>DEL</button>
