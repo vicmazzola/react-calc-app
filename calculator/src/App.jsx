f3a74d618a350689b095c7bbeb9af613dcc6af8e
Victor Mazzola <victormazzola2019@gmail.com>
2024-12-25 10:31:46 -0300

    add evaluate function

--- calculator/src/App.jsx
+++ calculator/src/App.jsx
@@ -16,18 +16,69 @@ export const ACTIONS = {
 function reducer(state, {type, payload}) {
     switch (type) {
         case ACTIONS.ADD_DIGIT:
-            if(payload.digit === "0" && state.currentOperand === "0") {
+            if (payload.digit === "0" && state.currentOperand === "0") {
                 return state
             }
-            if(payload.digit === "." && state.currentOperand.includes(".")) {
+            if (payload.digit === "." && state.currentOperand.includes(".")) {
                 return state
             }
             return {
                 ...state,
                 currentOperand: `${state.currentOperand || ""}${payload.digit}`,
             }
+        case ACTIONS.CHOOSE_OPERATION:
+            if (state.currentOperand == null && state.previousOperand == null) {
+                return state
+            }
+
+            if (state.currentOperand == null) {
+                return {
+                    ...state,
+                    operation: payload.operation,
+                }
+            }
+
+            if (state.previousOperand == null) {
+                return {
+                    ...state,
+                    operation: payload.operation,
+                    previousOperand: state.currentOperand,
+                    currentOperand: null,
+                }
+            }
+
+            return {
+                ...state,
+                previousOperand: evaluate(state),
+                operation: payload.operation,
+                currentOperand: null,
+            }
+        case ACTIONS.CLEAR:
+            return {}
     }
 }
+
+function evaluate({ currentOperand, previousOperand, operation}) {
+    const prev = parseFloat(previousOperand)
+    const current = parseFloat(currentOperand)
+    if (isNaN(prev) || isNaN(current)) return ""
+    let computation = ""
+    switch (operation) {
+        case "+":
+            computation = prev + current
+            break
+        case "-":
+            computation = prev - current
+            break
+        case "*":
+            computation = prev * current
+            break
+        case "÷":
+            computation = prev / current
+            break
+    }
+}
+
 
 function App() {
 
@@ -42,23 +93,23 @@ function App() {
                 <div className="previous-operand">{previousOperand} {operation}</div>
                 <div className="current-operand">{currentOperand}</div>
             </div>
-            <button className="span-two">AC</button>
+            <button className="span-two" onClick={() => dispatch({type: ACTIONS.CLEAR})}>AC</button>
             <button>DEL</button>
-            <OperationButton operation="÷" dispatch={dispatch} />
-            <DigitButton digit="1" dispatch={dispatch} />
-            <DigitButton digit="2" dispatch={dispatch} />
-            <DigitButton digit="3" dispatch={dispatch} />
-            <OperationButton operation="*" dispatch={dispatch} />
-            <DigitButton digit="4" dispatch={dispatch} />
-            <DigitButton digit="5" dispatch={dispatch} />
-            <DigitButton digit="6" dispatch={dispatch} />
-            <OperationButton operation="+" dispatch={dispatch} />
-            <DigitButton digit="7" dispatch={dispatch} />
-            <DigitButton digit="8" dispatch={dispatch} />
-            <DigitButton digit="9" dispatch={dispatch} />
-            <OperationButton operation="-" dispatch={dispatch} />
-            <DigitButton digit="." dispatch={dispatch} />
-            <DigitButton digit="0" dispatch={dispatch} />
+            <OperationButton operation="÷" dispatch={dispatch}/>
+            <DigitButton digit="1" dispatch={dispatch}/>
+            <DigitButton digit="2" dispatch={dispatch}/>
+            <DigitButton digit="3" dispatch={dispatch}/>
+            <OperationButton operation="*" dispatch={dispatch}/>
+            <DigitButton digit="4" dispatch={dispatch}/>
+            <DigitButton digit="5" dispatch={dispatch}/>
+            <DigitButton digit="6" dispatch={dispatch}/>
+            <OperationButton operation="+" dispatch={dispatch}/>
+            <DigitButton digit="7" dispatch={dispatch}/>
+            <DigitButton digit="8" dispatch={dispatch}/>
+            <DigitButton digit="9" dispatch={dispatch}/>
+            <OperationButton operation="-" dispatch={dispatch}/>
+            <DigitButton digit="." dispatch={dispatch}/>
+            <DigitButton digit="0" dispatch={dispatch}/>
             <button className="span-two">=</button>
 
         </div>
